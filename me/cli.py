eece610b7f55b549859c1d2755cb7536109a670a
Ryan Tuck <ryan@ryantuck.io>
2021-02-09 21:58:35 -0600

    config

--- me/cli.py
+++ me/cli.py
@@ -1,13 +1,29 @@
+import pathlib
+import yaml
+
 import click
+
+DEFAULT_CONFIG_FILEPATH = pathlib.Path("~/.me.yml").expanduser()
+
+
+def _config():
+    return yaml.safe_load(open(DEFAULT_CONFIG_FILEPATH))
+
 
 @click.group()
 def cli():
     pass
 
+
+@cli.command()
+def config():
+    print(yaml.dump(_config()))
+
+
 @cli.command()
 def hello():
-    print('hello')
+    print("hello")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="me",
     version="0.0.0",
-    install_requires=["Click"],
+    install_requires=["click", "pyyaml"],
     entry_points={"console_scripts": "me=me.cli:cli"},
 )
